98b5a02bedadb0cfecc049da14781bf5356cb276
Ivan Li <ivan.li@practiceinsight.io>
2017-09-16 15:09:53 +0800

    body parsers

--- app/controllers/ConfigController.scala
+++ app/controllers/ConfigController.scala
@@ -3,7 +3,7 @@ package controllers
 import javax.inject.{Inject, Singleton}
 
 import play.api.Configuration
-import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
+import play.api.mvc._
 
 @Singleton
 class ConfigController @Inject()(config: Configuration, cc: ControllerComponents) extends AbstractController(cc) {
@@ -23,10 +23,22 @@ class ConfigController @Inject()(config: Configuration, cc: ControllerComponents
   }
 
   def hello(to: String) = Action {
-    Ok(s"hello $to")
+    Ok(s"hello $to").as(HTML).withCookies(Cookie("k1", "v1"))
   }
 
   def redirect() = Action {
     Redirect(routes.ConfigController.hello("bob"))
+  }
+
+  // use curl -v -X POST localhost:9000/json -d '{"k1":"v1"}' -H "Content-Type: application/json to test
+  def json() = Action { implicit request: Request[AnyContent] =>
+    val body: AnyContent = request.body
+    val jsonBody = body.asJson
+
+    jsonBody.map { json =>
+      Ok("get :" + json.toString())
+    }.getOrElse(
+      BadRequest("invalid json format")
+    )
   }
 }
